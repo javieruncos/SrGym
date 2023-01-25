fc63d94bda5efd6a562122ca760a11129ebcc252
Javier Uncos <javiersrfc30@gmail.com>
2023-01-25 19:40:01 -0300

    agregamos la seccion de categorias

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -4,16 +4,16 @@ import { Nav, Navbar, Container } from "react-bootstrap";
 const Menu = () => {
   return (
     <div>
-      <Navbar bg="dark" expand="lg" className="navbar-dark">
+      <Navbar bg="dark" expand="lg" className="">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home" className="text-warning">React-Bootstrap</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mx-auto">
-              <Nav.Link href="#home" className="ms-4">Home</Nav.Link>
-              <Nav.Link href="#link" className="ms-4">Link</Nav.Link>
-              <Nav.Link href="#link" className="ms-4">Link</Nav.Link>
-              <Nav.Link href="#link" className="ms-4">Link</Nav.Link>
+              <Nav.Link href="#home" className="ms-4 text-warning">Home</Nav.Link>
+              <Nav.Link href="#link" className="ms-4 text-warning">Link</Nav.Link>
+              <Nav.Link href="#link" className="ms-4 text-warning">Link</Nav.Link>
+              <Nav.Link href="#link" className="ms-4 text-warning">Link</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/components/view/Inicio.jsx
+++ src/components/view/Inicio.jsx
@@ -5,18 +5,65 @@ const Inicio = () => {
   return (
     <div>
       {/*contenedor de la portada */}
-       <section>
+      <section>
         <article>
-            <div>
-                <div className="portada">
-
+          <div>
+            <div className="portada"></div>
+            <div className="descripcion row">
+              <div className="col-12 col-md-6">
+                <div className=" mt-4  w-100">
+                  <h1>titulo mas otra cosa</h1>
                 </div>
-                <div className="descripcion">
-
+              </div>
+              <div className="col-12 col-md-6">
+                <div
+                  className="d-flex justify-content-center align-items-center w-100"
+                  style={{ height: 200 }}
+                >
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id
+                    veniam corporis sint quibusdam assumenda nobis cupiditate,
+                    aspernatur natus voluptatum sequi odit culpa non impedit
+                    totam labore reprehenderit cum hic doloribus!
+                  </p>
                 </div>
+              </div>
             </div>
+          </div>
         </article>
-       </section>
+      </section>
+      <section className="container text-light">
+        <article>
+          <div>
+            <div>
+              <div className="text-center">
+                <h4 className="display-4">categorias tren superior</h4>
+              </div>
+            </div>
+          </div>
+          <div className="row text-warning mt-5">
+             <div className="col-12 col-md-4 col-lg-4">
+               <div className="divCategorias1">
+                <div>
+                <h3>PECTORALES</h3>
+                </div>
+               </div>
+             </div>
+             <div className="col-12 col-md-4 col-lg-4">
+              <div className="divCategorias2 ">
+                <div>
+                 <h3>ESPALDA</h3>
+                </div>
+              </div>
+             </div>
+             <div className="col-12 col-md-4 col-lg-4">
+              <div className="divCategorias3">
+              <h3>HOMBROS</h3>
+              </div>
+             </div>
+          </div>
+        </article>
+      </section>
     </div>
   );
 };

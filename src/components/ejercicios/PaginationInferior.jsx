fee93def7a0a4b272003062a0d4edffb00542adb
Javier Uncos <javiersrfc30@gmail.com>
2023-02-24 14:32:08 -0300

    cambios en las etiquetas a

--- src/components/ejercicios/PaginationInferior.jsx
+++ src/components/ejercicios/PaginationInferior.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Pagination = () => {
     return (
         <div className="d-flex justify-content-center mt-5">
           <ul className="text-light pagination pagination-md">
-            <li className="page-item bg-dark"><a href="/usuario/cliente" className="page-link bg-warning  text-dark">atras</a></li>
-            <li className="page-item "><a href="/cuadriceps" className="page-link bg-warning  text-dark">1</a></li>
-            <li className="page-item"><a href="/femoral" className="page-link bg-warning text-dark">2</a></li>
-            <li className="page-item"><a href="/gluteos" className="page-link bg-warning text-dark">3</a></li>
-            <li className="page-item"><a href="/pantorrillas" className="page-link bg-warning text-dark">4</a></li>
-            <li className="page-item"><a href="/aduptores" className="page-link bg-warning text-dark">5</a></li>
-            <li className="page-item"><a href="/pectorales" className="page-link bg-warning text-dark">tren superior</a></li>
+            <li className="page-item bg-dark"><Link to={"/usuario/cliente"} className="page-link bg-warning  text-dark">atras</Link></li>
+            <li className="page-item "><Link to={"/detalle/cuadriceps"} className="page-link bg-warning  text-dark">1</Link></li>
+            <li className="page-item"><Link to={"detalle/femoral"} className="page-link bg-warning text-dark">2</Link></li>
+            <li className="page-item"><Link to={"/gluteos"} className="page-link bg-warning text-dark">3</Link></li>
+            <li className="page-item"><Link to={"/pantorrillas"} className="page-link bg-warning text-dark">4</Link></li>
+            <li className="page-item"><Link to={"/aduptores"} className="page-link bg-warning text-dark">5</Link></li>
+            <li className="page-item"><Link to={"/pectorales"} className="page-link bg-warning text-dark">tren superior</Link></li>
           </ul>
       </div>
     );
--- src/components/ejercicios/PaginationSuperior.jsx
+++ src/components/ejercicios/PaginationSuperior.jsx
@@ -4,15 +4,18 @@ import React from 'react';
 const PaginationSuperior = () => {
     return (
         <div className="d-flex justify-content-center mt-5 ">
-        <ul className="text-light pagination pagination-md ">
+          <nav  aria-label="Page navigation example">
+        <ul className="text-light pagination pagination-md text-truncate">
           <li className="page-item bg-dark"><a href="/usuario/cliente" className="page-link bg-warning  text-dark">atras</a></li>
-          <li className="page-item "><a href="/pectorales" className="page-link bg-warning  text-dark">1</a></li>
-          <li className="page-item"><a href="/espalda" className="page-link bg-warning text-dark">2</a></li>
-          <li className="page-item"><a href="/hombros" className="page-link bg-warning text-dark">3</a></li>
-          <li className="page-item"><a href="/biceps" className="page-link bg-warning text-dark">4</a></li>
-          <li className="page-item"><a href="/triceps" className="page-link bg-warning text-dark">5</a></li>
-          <li className="page-item"><a href="/cuadriceps" className="page-link bg-warning text-dark">tren inferior</a></li>
+          <li className="page-item "><a href="/detalle/pectorales" className="page-link bg-warning  text-dark">1</a></li>
+          <li className="page-item"><a href="/detalle/espalda" className="page-link bg-warning text-dark">2</a></li>
+          <li className="page-item"><a href="/detalle/hombros" className="page-link bg-warning text-dark">3</a></li>
+          <li className="page-item"><a href="/detalle/biceps" className="page-link bg-warning text-dark">4</a></li>
+          <li className="page-item"><a href="/detalle/triceps" className="page-link bg-warning text-dark">5</a></li>
+          <li className="page-item"><a href="/detalle/cuadriceps" className="page-link bg-warning text-dark">tren inferior</a></li>
         </ul>
+
+          </nav>
     </div>
     );
 };

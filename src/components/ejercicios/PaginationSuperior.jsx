0787be04a481d1e3b38e9cf5452dc8934813ef62
Javier Uncos <javiersrfc30@gmail.com>
2023-02-23 23:32:48 -0300

    reemplazamos etiquetas a por link

--- src/components/ejercicios/PaginationSuperior.jsx
+++ src/components/ejercicios/PaginationSuperior.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const PaginationSuperior = () => {
     return (
         <div className="d-flex justify-content-center mt-5 ">
         <ul className="text-light pagination pagination-md ">
-          <li className="page-item bg-dark"><a href="/usuario/cliente" className="page-link bg-warning  text-dark">atras</a></li>
-          <li className="page-item "><a href="/pectorales" className="page-link bg-warning  text-dark">1</a></li>
-          <li className="page-item"><a href="/espalda" className="page-link bg-warning text-dark">2</a></li>
-          <li className="page-item"><a href="/hombros" className="page-link bg-warning text-dark">3</a></li>
-          <li className="page-item"><a href="/biceps" className="page-link bg-warning text-dark">4</a></li>
-          <li className="page-item"><a href="/triceps" className="page-link bg-warning text-dark">5</a></li>
-          <li className="page-item"><a href="/cuadriceps" className="page-link bg-warning text-dark">tren inferior</a></li>
+          <link className="page-item bg-dark"><Link to={"/usuario/cliente"} className="page-link bg-warning  text-dark">atras</Link></link>
+          <li className="page-item "><Link to={"/pectorales"} className="page-link bg-warning  text-dark">1</Link></li>
+          <li className="page-item"><Link to={"/espalda"} className="page-link bg-warning text-dark">2</Link></li>
+          <li className="page-item"><Link to={"/hombros"} className="page-link bg-warning text-dark">3</Link></li>
+          <li className="page-item"><Link to={"/biceps"} className="page-link bg-warning text-dark">4</Link></li>
+          <li className="page-item"><Link to={"/triceps"} className="page-link bg-warning text-dark">5</Link></li>
+          <li className="page-item"><Link to={"/cuadriceps"} className="page-link bg-warning text-dark">tren inferior</Link></li>
         </ul>
     </div>
     );

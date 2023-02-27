a349f5a2d99f6669924e5ca885e4df2ab0009f1a
Javier Uncos <javiersrfc30@gmail.com>
2023-02-26 23:46:34 -0300

    cambios en el tamaños de los planes en el inicio

--- src/components/ejercicios/PaginationSuperior.jsx
+++ src/components/ejercicios/PaginationSuperior.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const PaginationSuperior = () => {
-    return (
-        <div className="d-flex justify-content-center mt-5 ">
-          <nav  aria-label="Page navigation example">
+  return (
+    <div className="d-flex justify-content-center mt-5 ">
+      <nav aria-label="Page navigation example">
         <ul className="text-light pagination pagination-md text-truncate">
-          <li className="page-item bg-dark"><a href="/usuario/cliente" className="page-link bg-warning  text-dark">atras</a></li>
-          <li className="page-item "><a href="/detalle/pectorales" className="page-link bg-warning  text-dark">1</a></li>
-          <li className="page-item"><a href="/detalle/espalda" className="page-link bg-warning text-dark">2</a></li>
-          <li className="page-item"><a href="/detalle/hombros" className="page-link bg-warning text-dark">3</a></li>
-          <li className="page-item"><a href="/detalle/biceps" className="page-link bg-warning text-dark">4</a></li>
-          <li className="page-item"><a href="/detalle/triceps" className="page-link bg-warning text-dark">5</a></li>
-          <li className="page-item"><a href="/detalle/cuadriceps" className="page-link bg-warning text-dark">tren inferior</a></li>
+          <li className="page-item bg-dark"><Link to={"/usuario/cliente"} className="page-link bg-warning  text-dark">  atras</Link></li>
+          <li className="page-item "><Link to={"/detalle/pectorales"} className="page-link bg-warning  text-dark">  1</Link></li>
+          <li className="page-item"><Link to={"/detalle/espalda"} className="page-link bg-warning text-dark">2</Link></li>
+          <li className="page-item"><Link to={"/detalle/hombros"} className="page-link bg-warning text-dark">3</Link></li>
+          <li className="page-item"><Link to={"/detalle/biceps"} className="page-link bg-warning text-dark"> 4</Link></li>
+          <li className="page-item"><Link to={"/detalle/triceps"} className="page-link bg-warning text-dark"> 5</Link></li>
+          <li className="page-item"><Link to={"/detalle/cuadriceps"} className="page-link bg-warning text-dark"> tren inferior</Link></li>
         </ul>
-
-          </nav>
+      </nav>
     </div>
-    );
+  );
 };
 
 export default PaginationSuperior;
